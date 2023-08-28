acdc39b6c0f844dcf56c29ba1990cd3505e7169f
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2023-08-28 15:09:39 +0200

    notes from two workshops - monday

---
+++ my_notes_tidy1.R
@@ -0,0 +1,17 @@
+# the term geocomputation was coined at the university of Leeds
+
+# www.npt.scot online app for bike traffic modelling (where to build bike lanes?)
+
+# Author with Jakub Nowosad https://r.geocompx.org/
+
+pkgs = c(
+  "sf",
+  "tidyverse",
+  "geos",
+  "ggspatial",
+  "spData"
+)
+
+sapply(pkgs, require, character.only = TRUE)
+
+
